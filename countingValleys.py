7c094db4e115ae7c70223f59ea9c124d7d08266b
Denebaye <davidaddis4939@gmail.com>
2021-12-20 19:07:13 +0300

    countingValley's

---
+++ countingValleys.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valleys = 0
+    counter = 0
+    for step in path:
+        if step == 'D':
+            counter -= 1
+        else:
+            counter += 1
+            
+            
+        if counter == 0 and step == 'U' :
+            valleys += 1
+            
+        #print(counter , step, valleys)
+    return valleys                   
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
