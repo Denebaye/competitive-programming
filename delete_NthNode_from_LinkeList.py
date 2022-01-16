9311aaef2b173986561039db51872ada0b1e1afd
Denebaye <davidaddis4939@gmail.com>
2022-01-17 01:08:57 +0300

    elegent solution

--- delete_NthNode_from_LinkeList.py
+++ delete_NthNode_from_LinkeList.py
@@ -48,4 +48,27 @@ class Solution:
                 fEvenptr = fEvenptr.next.next
                 
             return head    
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        front = back = head
+        head = back
+        for i in range(n):
+            front = front.next
+        if not front:
+            return head.next
+        while front.next:
+            front = front.next
+            back = back.next
+        
+        back.next = back.next.next
+        return head
+        
+                    
+            
+                  
         
