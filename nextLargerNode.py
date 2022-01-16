1bcc2e0ee9247c7795d0b0cbc8533cb453325c9d
Denebaye <davidaddis4939@gmail.com>
2022-01-16 17:48:11 +0300

    startedtocodeusingvscodestudio

--- nextLargerNode.py
+++ nextLargerNode.py
@@ -1,23 +1,38 @@
-from logging.config import valid_ident
-
-
+#definition for class singly-linkedList
+from os import link
+from typing import *
 class Node:
-    def __init__(self, val , next = None) -> None:
-        self.value = val
+    def __init__(self,val = 0,next = None) -> None:
+        self.val = val
         self.next = next
 
-head = Node(5)
-cur = head
-ptr = head
-stack = []
-for i in range(10):
-    cur.next = Node(i)
-    cur = cur.next
-while ptr != None:
-    stack.append(ptr)
-    print(stack[-1].value)
-    ptr = ptr.next
-    
+class ListNode:
+    def __init__(self,head) -> None:
+        self.head = head
 
+    def nextLargerNodes(self) -> List[int]:
+        pos = -1
+        stack = []
+        ans = []
+        node = self.head
+        while node:
+            pos += 1
+            ans.append(0)
+            while stack and stack[-1][1] < node.val:
+                idx,val = stack.pop()
+                ans[idx] = node.val
 
+            stack.append((pos,node.val))
+            node = node.next
+        return ans
 
+arr = [6,1,1,3,5]
+head = Node(arr[0])
+linked_list = ListNode(head)
+node = linked_list.head
+
+for i in range(1, len(arr)):
+    node.next = Node(arr[i])
+    node = node.next
+
+print(linked_list.nextLargerNodes())
