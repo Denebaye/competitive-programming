37930f2c2bcc9718d5467d5c642e99e26b0aa3ea
Denebaye <davidaddis4939@gmail.com>
2022-01-31 10:55:49 +0300

    using stack it's done

---
+++ validatestack.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = 0
+        j = 0
+        while i < len(pushed):
+            if popped[j] == pushed[i]:
+                j += 1
+                while stack and j < len(popped) and stack[-1] == popped[j]:
+                    stack.pop()
+                    j += 1
+
+            else:
+                stack.append(pushed[i])
+
+            i += 1
+
+        return len(stack) == 0
