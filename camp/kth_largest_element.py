3046b64874668ba26c415b56cdfc136a9ea3b2ee
Denebaye <davidaddis4939@gmail.com>
2022-02-25 13:22:55 +0300

    the same question as the previous one and done using heap

--- camp/kth_largest_element.py
+++ camp/kth_largest_element.py
@@ -1,7 +1,11 @@
 class Solution:
+   class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
+        """heapq.heapify(nums)
         while len(nums) > k:
             heapq.heappop(nums)
         
         return heapq.heappop(nums)
+        """
+        nums = sorted(nums,reverse = True)
+        return nums[k - 1]
