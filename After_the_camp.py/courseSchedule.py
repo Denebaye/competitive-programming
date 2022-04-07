a47973ef78e1d280565c14f15ef2bd483745b178
Denebaye <davidaddis4939@gmail.com>
2022-04-08 01:16:40 +0300

    dfs and bfs solution

--- After_the_camp.py/courseSchedule.py
+++ After_the_camp.py/courseSchedule.py
@@ -1,30 +1,47 @@
 # from collection import deque
-class Solution:
+class Solution:   
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        indegree = [0]*numCourses
+        # indegree = [0]*numCourses
         
         
-        for i in range(len(prerequisites)):
-            des,src = prerequisites[i]
-            indegree[des] += 1
+        for des,src in prerequisites:
+            # indegree[des] += 1
             graph[src].append(des)
-        
-        queue = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-        count = 0
-        while queue:
-            curr = queue.popleft()
-            count += 1
             
-            for child in graph[curr]:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    queue.append(child)
+        def cycle(node,tracker,visited):
+            tracker[node] = True
+            visited[node] = True
+            for n in graph[node]:
+                if n not in visited and cycle(n,tracker,visited):
+                    return True
+                if n in tracker:
+                    return True
+            tracker.pop(node)
+            return False
+        visited = {}
+        for course in range(numCourses):
+            tracker = {}
+            if course not in visited and cycle(course,tracker,visited):
+                return False
         
-        return count == numCourses
+        return True
+        
+#         queue = deque()
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 queue.append(i)
+#         count = 0
+#         while queue:
+#             curr = queue.popleft()
+#             count += 1
+            
+#             for child in graph[curr]:
+#                 indegree[child] -= 1
+#                 if indegree[child] == 0:
+#                     queue.append(child)
+        
+#         return count == numCourses
         
                 
             
