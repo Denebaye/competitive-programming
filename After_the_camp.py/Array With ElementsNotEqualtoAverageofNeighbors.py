6c168a88474d422c92c03e59c4166b0091e1a061
Denebaye <davidaddis4939@gmail.com>
2022-04-20 18:53:17 +0300

    wiggle sort and it's tricky

--- After_the_camp.py/Array With ElementsNotEqualtoAverageofNeighbors.py
+++ After_the_camp.py/Array With ElementsNotEqualtoAverageofNeighbors.py
@@ -2,10 +2,12 @@ class Solution(object):
     def rearrangeArray(self, nums):
         
         nums = sorted(nums)
-        for i in range(1, len(nums), 2):
-            if i != len(nums)-1:
-                nums[i], nums[i+1] = nums[i+1], nums[i]
-            
+
+        i = 1
+        while i < len(nums) - 1:
+            nums[i],nums[i + 1] = nums[i + 1],nums[i]
+            i += 2
+ 
         return nums
 # space and time complexity
 # time = O(nlogn)
