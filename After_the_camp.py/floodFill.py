dc32f67d8c145669f2a784ece629610371296153
Denebaye <davidaddis4939@gmail.com>
2022-05-30 11:53:31 +0300

    [dfs has been opened]

---
+++ After_the_camp.py/floodFill.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dfs(self,image,sr,sc,newColor,visited):
+        visited.add((sr,sc))
+        oldColor = image[sr][sc]
+        image[sr][sc] = newColor
+        
+        directions = [(sr - 1,sc),(sr + 1,sc),(sr,sc - 1),(sr,sc + 1)]
+        for dir in directions:
+            
+            if dir not in visited and (dir[0] < len(image)) and (dir[1] < len(image[0])) and (dir[1] >=0) and (dir[0] >= 0)and image[dir[0]][dir[1]] == oldColor:
+    
+                self.dfs(image,dir[0],dir[1],newColor,visited)
+                
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        mySet = set()
+        self.dfs(image,sr,sc,newColor,mySet)
+        return image
