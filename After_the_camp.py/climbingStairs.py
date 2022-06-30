01f824aef4b9268a84e12eabaa3c2cf8cdae20e4
Denebaye <davidaddis4939@gmail.com>
2022-07-01 02:12:50 +0300

    another simple dp question

---
+++ After_the_camp.py/climbingStairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        last = 1
+        prev = 1
+        for _ in range(n - 1):
+            temp = prev
+            prev = last + prev
+            last = temp
+        
+        return prev
